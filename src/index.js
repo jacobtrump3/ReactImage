c058e5a9ad04f65450e43863339aeb99e6d6d835
Byron Hall <byron.hall@infosys.com>
2018-07-15 13:42:57 -0500

    bg image

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css'
 
 var pinchZoom = require('pinch-zoom');
 
 class Index extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
         pinchZoom(
             document.getElementById('wrapper')
         );
     }
     render() {
         return (
-                <div>
-                    <img id="wrapper" src="https://cdn-images-1.medium.com/fit/c/120/120/1*lKN9xV1YEin-2wfAiGySBA.png" alt="" />
-                </div>
+            <img id="wrapper" src="https://cdn-images-1.medium.com/fit/c/120/120/1*lKN9xV1YEin-2wfAiGySBA.png" alt="" />
         );
     }
 }
