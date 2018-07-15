a561f580109c5057c87aa7a9cf7325a73cb04f96
Byron Hall <byron.hall@infosys.com>
2018-07-15 15:22:41 -0500

    tap reset

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ import ImageZoom from 'react-medium-image-zoom';
 class Index extends React.Component {
     componentDidMount() {
         pinchZoom(
-            document.getElementById('react-img'),
+            document.getElementById('react-img')
+        );
+
+        pinchZoom(
+            document.getElementById('react-img-tap'),
             {
                 tapreset: true
             }
@@ -21,10 +25,15 @@ class Index extends React.Component {
             <span>
                 <h1>Module: pinch-zoom</h1>
                 <div id="img-container">
-                    <img id="react-img" src="https://cdn-images-1.medium.com/fit/c/120/120/1*lKN9xV1YEin-2wfAiGySBA.png" alt="React" />
+                    <img id="react-img-tap" src="https://cdn-images-1.medium.com/fit/c/120/120/1*lKN9xV1YEin-2wfAiGySBA.png" alt="React" />
                 </div>
                 <hr />
-                <h1>Module: react-medium-image-zoom</h1>
+                <h2>Module: pinch-zoom (with single tap reset)</h2>
+                <div id="img-container">
+                    <img id="react-img-tap" src="https://cdn-images-1.medium.com/fit/c/120/120/1*lKN9xV1YEin-2wfAiGySBA.png" alt="React" />
+                </div>
+                <hr />
+                <h3>Module: react-medium-image-zoom</h3>
                 <ImageZoom 
                     image={{
                         src: 'http://www.novasoftware.com/Images/Case-Study/JavaScript/html5.png'
