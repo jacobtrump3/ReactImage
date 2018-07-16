81086a37da6a8ae1fdbb085c392f220ec54beec7
Byron Hall <byron.hall@infosys.com>
2018-07-16 13:53:34 -0500

    react-touch-events

--- src/index.js
+++ src/index.js
@@ -7,56 +7,57 @@ import ImageZoom from 'react-medium-image-zoom';
 
 import ReactImageMagnify from 'react-image-magnify';
 
-import $ from 'jquery';
+import ReactTouchEvents from "react-touch-events";
 
 class Index extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            mouse: false,
-            touch: true
+            mouse: true,
+            touch: false
         }
     }
 
-    componentDidMount(){
-        $(document).mousemove(() => {
-            this.setState({
-                mouse: true,
-                touch: false
-            });
-            $(document).off('mousemove');
+    handleTap() {
+        this.setState({
+            mouse: false,
+            touch: true
         });
     }
     render() {
         return (
-            <span>
-                Touch detected: {this.state.touch ? 'true' : 'false'}<br />
-                Mouse detected: {this.state.mouse ? 'true' : 'false'}
-                <hr />
-                <h1>A combination based on touch or mouse</h1>
-                {this.state.mouse &&
-                    <ReactImageMagnify {...{
-                        smallImage: {
-                            src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
-                            width: 240,
-                            height: 240
-                        },
-                        largeImage: {
-                            src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
-                            width: 480,
-                            height: 480
-                        }
-                    }} />
-                }
-                {this.state.touch &&
-                    <ImageZoom
-                        image={{
-                            src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg'
-                        }}
-                    />
-                }
-            </span>
+            <ReactTouchEvents
+                onTap={this.handleTap.bind(this)}
+            >
+                <span>
+                    Touch detected: {this.state.touch ? 'true' : 'false'}<br />
+                    Mouse detected: {this.state.mouse ? 'true' : 'false'}
+                    <hr />
+                    <h1>A combination based on touch or mouse</h1>
+                    {this.state.mouse &&
+                        <ReactImageMagnify {...{
+                            smallImage: {
+                                src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
+                                width: 240,
+                                height: 240
+                            },
+                            largeImage: {
+                                src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
+                                width: 480,
+                                height: 480
+                            }
+                        }} />
+                    }
+                    {this.state.touch &&
+                        <ImageZoom
+                            image={{
+                                src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg'
+                            }}
+                        />
+                    }
+                </span>
+            </ReactTouchEvents>
         );
     }
 }
