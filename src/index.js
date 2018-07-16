2fad688450d3ad6ea8bf56a48af2500408f9c5d8
Byron Hall <byron.hall@infosys.com>
2018-07-16 15:17:55 -0500

    some attributes2

--- src/index.js
+++ src/index.js
@@ -7,26 +7,24 @@ import ReactImageMagnify from 'react-image-magnify';
 
 class Index extends React.Component {
     render() {
-        console.log(window.innerHeight);
-        console.log(window.innerWidth);
         return (
             <span>
-                enlargedImageContainerStyle
+                enlargedImageContainerStyle take 2
                 <ReactImageMagnify {...{
                     smallImage: {
                         src: 'https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414',
                         width: 240,
                         height: 240,
-                        enlargedImageContainerStyle : {
-                            width: window.innerWidth,
-                            height: window.innerHeight
-                        }
                     },
                     largeImage: {
                         src: 'https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414',
                         width: 760,
-                        height: 760
-                    }
+                        height: 760,
+                    },
+                    enlargedImageContainerStyle: {
+                        width: window.innerWidth,
+                        height: window.innerHeight
+                    },
                 }} />
             </span>
         );
