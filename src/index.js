7a6fbc2b9791d10a05309417d30a3fc2f8cefed3
Byron Hall <byron.hall@infosys.com>
2018-07-16 14:19:19 -0500

    one

--- src/index.js
+++ src/index.js
@@ -3,61 +3,26 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-import ImageZoom from 'react-medium-image-zoom';
-
 import ReactImageMagnify from 'react-image-magnify';
 
-import ReactTouchEvents from "react-touch-events";
-
 class Index extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            mouse: true,
-            touch: false
-        }
-    }
-
-    handleTap() {
-        this.setState({
-            mouse: false,
-            touch: true
-        });
-    }
     render() {
         return (
-            <ReactTouchEvents
-                onTap={this.handleTap.bind(this)}
-            >
-                <span>
-                    Touch detected: {this.state.touch ? 'true' : 'false'}<br />
-                    Mouse detected: {this.state.mouse ? 'true' : 'false'}
-                    <hr />
-                    <h1>A combination based on touch or mouse</h1>
-                    {this.state.mouse &&
-                        <ReactImageMagnify {...{
-                            smallImage: {
-                                src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
-                                width: 240,
-                                height: 240
-                            },
-                            largeImage: {
-                                src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
-                                width: 480,
-                                height: 480
-                            }
-                        }} />
+            <span>
+                <ReactImageMagnify {...{
+                    smallImage: {
+                        src: 'https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414',
+                        width: 240,
+                        height: 240,
+                        isActivatedOnTouch: true
+                    },
+                    largeImage: {
+                        src: 'https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414',
+                        width: 760,
+                        height: 760
                     }
-                    {this.state.touch &&
-                        <ImageZoom
-                            image={{
-                                src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg'
-                            }}
-                        />
-                    }
-                </span>
-            </ReactTouchEvents>
+                }} />
+            </span>
         );
     }
 }
