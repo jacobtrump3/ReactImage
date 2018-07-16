4e154473818203d1d9d7ab39b8efa3aa5e357d64
Byron Hall <byron.hall@infosys.com>
2018-07-16 12:30:37 -0500

    added boolean

--- src/index.js
+++ src/index.js
@@ -8,9 +8,29 @@ import ImageZoom from 'react-medium-image-zoom';
 import ReactImageMagnify from 'react-image-magnify';
 
 class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mouse: true,
+            touch: false
+        };
+        document.getElementById('body').addEventListener('touchstart', this.updatePage);
+    }
+
+    updatePage() {
+        this.setState({
+            mouse: false,
+            touch: true
+        });
+        document.getElementById('body').removeEventListener('touchstart', () => { });
+    }
+
     render() {
         return (
             <span>
+                Touch detected: {this.state.touch ? 'true' : 'false'}<br />
+                Mouse detected: {this.state.mouse ? 'true' : 'false'}
+                <hr />
                 <h1>Module: react-medium-image-zoom</h1>
                 <ImageZoom
                     image={{
@@ -31,6 +51,29 @@ class Index extends React.Component {
                         height: 480
                     }
                 }} />
+                <hr />
+                <h3>A combination based on touch or mouse</h3>
+                {this.state.mouse &&
+                    <ReactImageMagnify {...{
+                        smallImage: {
+                            src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
+                            width: 240,
+                            height: 240
+                        },
+                        largeImage: {
+                            src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg',
+                            width: 480,
+                            height: 480
+                        }
+                    }} />
+                }
+                {this.state.touch &&
+                    <ImageZoom
+                        image={{
+                            src: 'https://pbs.twimg.com/profile_images/827354992377860096/sUe4dG_L_400x400.jpg'
+                        }}
+                    />
+                }
             </span>
         );
     }
