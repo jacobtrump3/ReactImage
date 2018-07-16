6feefe6f786d704d636fe9e2de49d9628e0d8db9
Byron Hall <byron.hall@infosys.com>
2018-07-16 11:51:17 -0500

    react-image-magnify

--- src/index.js
+++ src/index.js
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-var pinchZoom = require('pinch-zoom');
-
 import ImageZoom from 'react-medium-image-zoom';
 
+import ReactImageMagnify from 'react-image-magnify';
+
 class Index extends React.Component {
-    componentDidMount() {
-        pinchZoom(
-            document.getElementById('react-img'),
-            {
-                tapreset : true
-            }
-        );
-    }
     render() {
         return (
             <span>
-                <h1>Module: pinch-zoom</h1>
-                <div id="img-container">
-                    <img id="react-img" src="https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414" alt="React" />
-                </div>
-                <hr />
-                <h2>Module: react-medium-image-zoom</h2>
-                <ImageZoom 
+                <h1>Module: react-medium-image-zoom</h1>
+                <ImageZoom
                     image={{
                         src: 'https://frontendmasters.com/assets/html5css3.png'
                     }}
                 />
                 <hr />
+                <h2>Module: react-image-magnify</h2>
+                <ReactImageMagnify {...{
+                    smallImage: {
+                        src: 'https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414',
+                        width: 240,
+                        height: 240
+                    },
+                    largeImage: {
+                        src: 'https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414',
+                        width: 480,
+                        height: 480
+                    }
+                }} />
             </span>
         );
     }
